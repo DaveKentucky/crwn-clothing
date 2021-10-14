0300f23aafa72c45b86ec639be8d3a46516b2ab1
DaveKentucky <didam7@wp.pl>
2021-10-14 18:30:20 +0200

    Added redux, implemented userReducer and userActions to header component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -24,7 +24,10 @@ class SignIn extends React.Component {
 
         try {
             await auth.signInWithEmailAndPassword(email, password);
-            this.setState({ email: '', password: '' });
+            this.setState({
+                email: '',
+                password: ''
+            });
         } catch (error) {
             console.error(error);
         }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 
+import store from './redux/store';
+
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <Provider store={ store }>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
 );
