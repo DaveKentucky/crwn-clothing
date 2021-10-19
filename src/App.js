6cbb0a881700d65315fad2cb0f48335c10f29bae
DaveKentucky <didam7@wp.pl>
2021-10-19 19:32:10 +0200

    Added chekout page and checkout item component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+// libraries
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-
+// styling
 import './App.css';
-
+// pages
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+// components
+import Header from './components/header/header.component';
+// firebase
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+// redux
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors'
 
@@ -46,6 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={ HomePage } />
           <Route path='/shop' component={ ShopPage } />
+          <Route exact path='/checkout' component={ CheckoutPage } />
           <Route exact path='/sign-in' render={ () => this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage /> } />
         </Switch>
       </div>
