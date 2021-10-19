e0c5ae0733e3d8031654a986749d318997f94f66
DaveKentucky <didam7@wp.pl>
2021-10-19 17:28:27 +0200

    Added the items quantity selector for the cart icon

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -11,7 +11,7 @@ const CartDropdown = ({ cartItems }) => (
         <div className='cart-items'>
             {
                 cartItems.map(cartItem =>
-                    <CartItem key={ cartItem.id } item={ cartItem }/>)
+                    <CartItem key={ cartItem.id } item={ cartItem }/>) 
             }
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
