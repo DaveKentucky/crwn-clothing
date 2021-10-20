b56bf22ee87bd586b4d878230ab769f99aa70875
DaveKentucky <didam7@wp.pl>
2021-10-20 13:32:09 +0200

    Added redux persistor for storing cart items

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-// styling
+// styles
 import './App.css';
 // pages
 import HomePage from './pages/homepage/homepage.component';
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+// libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import { PersistGate } from 'redux-persist/integration/react';
+// styles
 import './index.css';
+// components
 import App from './App';
-
-import store from './redux/store';
+// redux
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={ store }>
     <HashRouter>
-      <App />
+      <PersistGate persistor={ persistor }>
+        <App />
+      </PersistGate>
     </HashRouter>
   </Provider>,
   document.getElementById('root')
