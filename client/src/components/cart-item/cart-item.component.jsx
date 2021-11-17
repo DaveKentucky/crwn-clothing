eea1353baa4ddc3cc6f3b0ce1f453ddadcdf4425
DaveKentucky <didam7@wp.pl>
2021-11-17 11:57:41 +0100

    Memoized the cart item component

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -14,6 +14,6 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
             </span>
         </ItemDetailsContainer>
     </CartItemContainer>
-)
+);
 
-export default CartItem;
+export default React.memo(CartItem);
